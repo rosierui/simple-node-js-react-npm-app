ce3b89aa3ffd610f03168e6a9a97e2cd4ccdb435
moonwave <moonwave.systems@gmail.com>
2021-03-21 18:46:12 -0700

    deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,10 +4,6 @@ echo $0
 echo "whoami: " `whoami`
 echo "pwd: " `pwd`
 
-echo "=== set ===" `set`
-set
-echo "=== env ==="
-env
 echo 'The following "npm" command builds your Node.js/React application for'
 echo 'production in the local "build" directory (i.e. within the'
 echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
@@ -16,6 +12,17 @@ echo 'the best performance.'
 set -x
 npm run build
 set +x
+
+echo "=== set ==="
+set
+
+echo
+echo "=== env ==="
+env
+
+pwd
+
+whoami
 
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
@@ -26,13 +33,10 @@ echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-echo "cat /etc/os-release"
 cat /etc/os-release
 
-echo "uname -a"
 uname -a
 
-echo "which npm" `which npm`
 npm --version
 npm version
 
@@ -40,6 +44,10 @@ npm start &
 sleep 1
 echo $! > .pidfile
 set +x
+
+pwd
+ls -l
+cat .pidfile
 
 echo 'Now...'
 echo 'Visit http://localhost:3000 or http://localhost:3500 to see your Node.js/React application in action.'
